d0de821e6b275b4f8bf88783cb1798d05da19faf
Naman <110986742+officialDeveloper01@users.noreply.github.com>
2023-11-26 13:06:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+let search = document.querySelector('.searchbar');
+
+document.querySelector('#search').onclick = () => {
+    search.classList.toggle('active');
+}
+
+
+
+
+let favourite = document.querySelector('.favourite');
+
+document.querySelector('#favourite').onclick = () => {
+    favourite.classList.toggle('active');
+}
+
+
+
+let checkout = document.querySelector('.checkout');
+
+document.querySelector('#checkout').onclick = () => {
+    checkout.classList.toggle('active');
+}
+
+
+
+let login = document.querySelector('.login');
+
+document.querySelector('#login').onclick = () => {
+    login.classList.toggle('active');
+}
